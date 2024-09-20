b6962881c31418f390150a3d03ca78d1c2972b80
KAMILA KARAMANOVA <tp051798@mail.apu.edu.my>
2024-09-20 12:26:52 +0800

    destructured the App component & created separate components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+  return (
+    <div className="d-flex flex-column">
+      <h1>About me</h1>
+      <p>
+        I hold a Dual Bachelor’s in Software Engineering (2018-2022) and a
+        Master’s in Business Administration (2022-2023) from Staffordshire
+        University & Asia Pacific University. With over three years of web
+        development experience, I specialize in front-end technologies. As a Web
+        Developer/Team Lead at Admin Apspace, I led the migration of legacy
+        systems into a new Angular-based web app, developed APIs using Flask,
+        and managed a team of four developers. In my prior role at the
+        Multisites team, I built automated workflows and responsive websites
+        using WordPress, PHP, and JavaScript.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Greeting.js
@@ -0,0 +1,45 @@
+import helloImg from "../assets/hello-img.png";
+
+export default function Greeting() {
+  return (
+    <div className="d-flex vh-100">
+      <div className="col my-auto ms-5">
+        <h1 class="display-2 fw-normal">
+          Hello,
+          <br /> I'm Kamila.
+        </h1>
+        <p className="fw-normal lh-sm fs-5">
+          I am a recent graduate with over three years of experience in web
+          development, specialising in front-end technologies. If you have an
+          exiting project, feel free to connect with me on{" "}
+          <a
+            class="link-dark link-offset-1"
+            href="https://www.linkedin.com/in/kamila-karamanova/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>{" "}
+          or hit me up via{" "}
+          <a
+            class="link-dark link-offset-1"
+            href="mailto:karamanova.2000@gmail.com"
+            target="_blank"
+          >
+            email
+          </a>
+          !
+        </p>
+        <a
+          href="https://drive.google.com/file/d/1hzkqzpIbDbFFWx8C6hmKOxf71ENBhI8l/view?usp=sharing"
+          target="_blank"
+          className="btn btn-outline-dark"
+        >
+          Check my CV
+        </a>
+      </div>
+      <div className="col d-flex my-auto justify-content-center">
+        <img src={helloImg} alt="hand-waving memoji" />
+      </div>
+    </div>
+  );
+}
