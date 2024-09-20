5657144dfde8e0844cba4ba717b6aeac45c4a2c6
KAMILA KARAMANOVA <tp051798@mail.apu.edu.my>
2024-09-20 16:33:10 +0800

    added the fonts, margin to the text and rounded borders to the button

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,16 +4,16 @@ export default function Greeting() {
   return (
     <div className="d-flex vh-100">
       <div className="col my-auto ms-5">
-        <h1 class="display-2 fw-normal">
+        <h2 className="display-2 fw-normal greeting-header">
           Hello,
           <br /> I'm Kamila.
-        </h1>
-        <p className="fw-normal lh-sm fs-5">
+        </h2>
+        <p className="fw-normal lh-base fs-5 intro-text">
           I am a recent graduate with over three years of experience in web
           development, specializing in front-end technologies. If you have an
           exciting project, feel free to connect with me on{" "}
           <a
-            class="link-dark link-offset-1"
+            className="link-dark link-offset-1"
             href="https://www.linkedin.com/in/kamila-karamanova/"
             target="_blank"
           >
@@ -21,7 +21,7 @@ export default function Greeting() {
           </a>{" "}
           or hit me up via{" "}
           <a
-            class="link-dark link-offset-1"
+            className="link-dark link-offset-1"
             href="mailto:karamanova.2000@gmail.com"
             target="_blank"
           >
@@ -29,13 +29,15 @@ export default function Greeting() {
           </a>
           !
         </p>
-        <a
-          href="https://drive.google.com/file/d/1hzkqzpIbDbFFWx8C6hmKOxf71ENBhI8l/view?usp=sharing"
-          target="_blank"
-          className="btn btn-outline-dark"
-        >
-          Check my CV
-        </a>
+        <div className="justify-item-center">
+          <a
+            href="https://drive.google.com/file/d/1hzkqzpIbDbFFWx8C6hmKOxf71ENBhI8l/view?usp=sharing"
+            target="_blank"
+            className="btn btn-lg btn-outline-dark rounded-pill button-text"
+          >
+            Check my CV
+          </a>
+        </div>
       </div>
       <div className="col d-flex my-auto justify-content-center">
         <img src={helloImg} alt="hand-waving memoji" />
