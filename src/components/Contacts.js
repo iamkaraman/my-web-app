97efe97f32a03800adb4fb3b34aa6e150564aff9
KAMILA KARAMANOVA <tp051798@mail.apu.edu.my>
2024-09-27 01:48:57 +0800

    added some logos

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,3 +1,7 @@
+import linkedImg from "../assets/linkedIn-logo.png";
+import githubImg from "../assets/github-logo-removebg-preview.png";
+import emailImg from "../assets/email-logo-removebg-preview.png";
+
 export default function Contacts() {
   return (
     <div
@@ -5,40 +9,28 @@ export default function Contacts() {
       style={{ height: "50vh" }}
     >
       <h1 className="paragraph-header">Contacts</h1>
-      <div className="row">
-        <div className="col">
-          <p className="fw-normal lh-base fs-5 paragraph-text">
-            <a
-              className="link-dark link-offset-1"
-              href="https://www.linkedin.com/in/kamila-karamanova/"
-              target="_blank"
-            >
-              LinkedIn
-            </a>
-          </p>
-        </div>
-        <div className="col">
-          <p className="fw-normal lh-base fs-5 paragraph-text">
-            <a
-              className="link-dark link-offset-1"
-              href="https://github.com/iamkaraman"
-              target="_blank"
-            >
-              GitHub
-            </a>
-          </p>
-        </div>
-        <div className="col">
-          <p className="fw-normal lh-base fs-5 paragraph-text">
-            <a
-              className="link-dark link-offset-1"
-              href="mailto:karamanova.2000@gmail.com"
-              target="_blank"
-            >
-              Email
-            </a>
-          </p>
-        </div>
+      <div className=" d-flex gap-3 justify-content-center">
+        <a
+          className="link-dark link-offset-1"
+          href="https://www.linkedin.com/in/kamila-karamanova/"
+          target="_blank"
+        >
+          <img src={linkedImg} alt="linkedIn logo" style={{ width: "50px" }} />
+        </a>
+        <a
+          className="link-dark link-offset-1"
+          href="https://github.com/iamkaraman"
+          target="_blank"
+        >
+          <img src={githubImg} alt="linkedIn logo" style={{ width: "50px" }} />
+        </a>
+        <a
+          className="link-dark link-offset-1"
+          href="mailto:karamanova.2000@gmail.com"
+          target="_blank"
+        >
+          <img src={emailImg} alt="linkedIn logo" style={{ width: "50px" }} />
+        </a>
       </div>
     </div>
   );
